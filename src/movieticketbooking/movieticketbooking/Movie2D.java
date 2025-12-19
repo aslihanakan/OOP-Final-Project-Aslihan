0b6ed03bdd62093e7b455f3030f44d2d1ea2cc58
aslih <aslih@Jarvis>
2025-12-19 23:45:49 +0300

    The package structure has been fixed and the Movie2D class has been updated.

---
+++ src/movieticketbooking/movieticketbooking/Movie2D.java
@@ -0,0 +1,12 @@
+package movieticketbooking;
+public class Movie2D extends Movie {
+
+    public Movie2D(String title, int duration) {
+        super(title, duration);
+    }
+
+    @Override
+    public double getPrice() {
+        return 200.0;
+    }
+}
